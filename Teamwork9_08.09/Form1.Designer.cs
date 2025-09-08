bfc1d6dc8158aa0c3588a7e3be272f8716bf65a3
hthuong2595 <thuongqlht02@gmail.com>
2025-09-08 15:26:40 +0700

    hthuong-commit
    Add UI for quadratic equation solver
    
    Implemented a user interface in the Windows Forms application for solving quadratic equations. Updated `Form1.Designer.cs` to include labels, text boxes, and buttons for user input and results. Modified `Form1.cs` to add event handlers and logic for calculating roots based on user input. Created a new resource file (`Form1.resx`) for localization and resource management.

--- Teamwork9_08.09/Form1.Designer.cs
+++ Teamwork9_08.09/Form1.Designer.cs
@@ -29,13 +29,154 @@ namespace Teamwork9_08._09
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.btnok = new System.Windows.Forms.Button();
+            this.btnend = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblkq1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(285, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Giải phương trình bậc 2";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập a";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nhập c";
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(169, 77);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 20);
+            this.txta.TabIndex = 4;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(169, 115);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.TabIndex = 5;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(169, 152);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 20);
+            this.txtc.TabIndex = 6;
+            // 
+            // btnok
+            // 
+            this.btnok.Location = new System.Drawing.Point(111, 204);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(153, 41);
+            this.btnok.TabIndex = 7;
+            this.btnok.Text = "Giải phương trình";
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // btnend
+            // 
+            this.btnend.Location = new System.Drawing.Point(694, 248);
+            this.btnend.Name = "btnend";
+            this.btnend.Size = new System.Drawing.Size(95, 28);
+            this.btnend.TabIndex = 8;
+            this.btnend.Text = "Đóng";
+            this.btnend.UseVisualStyleBackColor = true;
+            this.btnend.Click += new System.EventHandler(this.btnend_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(435, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Kết quả";
+            // 
+            // lblkq1
+            // 
+            this.lblkq1.AutoSize = true;
+            this.lblkq1.Location = new System.Drawing.Point(541, 80);
+            this.lblkq1.Name = "lblkq1";
+            this.lblkq1.Size = new System.Drawing.Size(22, 13);
+            this.lblkq1.TabIndex = 12;
+            this.lblkq1.Text = "KQ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblkq1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnend);
+            this.Controls.Add(this.btnok);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Button btnok;
+        private System.Windows.Forms.Button btnend;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblkq1;
     }
 }
 
--- Teamwork9_08.09/Form1.cs
+++ Teamwork9_08.09/Form1.cs
@@ -12,9 +12,57 @@ namespace Teamwork9_08._09
 {
     public partial class Form1 : Form
     {
+        double a, b, c, d, x1, x2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnend_Click(object sender, EventArgs e)
+        {
+            this.Close();//ket thuc form nay dong form
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            a=Convert.ToDouble(txta.Text);
+            b=Convert.ToDouble(txtb.Text);
+            c=Convert.ToDouble(txtc.Text);
+            d=b*b-4*a*c;
+            if (d<0)
+            {
+                lblkq1.Text ="Phuong trinh vo nghiem";
+                //MessageBox.Show("Phuong trinh vo nghiem");
+            }
+            else if (d==0)
+            {
+                x1 = -b / (2 * a);
+                lblkq1.Text="Phuong trinh co nghiem kep x1=x2="+Math.Round(x1, 1);
+                //MessageBox.Show("Phuong trinh co nghiem kep x1=x2="+x1);
+            }
+            else
+            {
+                x1=(-b+Math.Sqrt(d))/(2*a);
+                x2=(-b-Math.Sqrt(d))/(2*a);
+                lblkq1.Text="Phuong trinh co 2 nghiem phan biet:\n x1="+ Math.Round(x1, 1) + "\n x2="+ Math.Round(x2, 1);
+                //MessageBox.Show("Phuong trinh co 2 nghiem phan biet:\n x1="+x1+"\n x2="+x2);
+            }
         }
     }
 }
